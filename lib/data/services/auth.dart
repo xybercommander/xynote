0eb65eef7b639d668f2837043a5a1404afcf07e0
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2022-04-29 12:14:05 +0530

    Adding auth methods

---
+++ lib/data/services/auth.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:xynote/data/models/user_profile_model.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  UserProfile? _userFromFirebaseUser(User user) {
+    return UserProfile(userId: user.uid);
+  }
+
+  //--------- SIGN UP ---------//
+  Future<UserProfile?> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User user = result.user!;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+    }
+  }
+}
