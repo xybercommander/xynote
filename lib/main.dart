7f317493850ae83056622ad36c93277060b0d7b4
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2022-04-29 11:25:35 +0530

    Added new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demo',   
       theme: ThemeData(        
         primarySwatch: Colors.blue,
+        fontFamily: 'RobotoMedium'
       ),
       home: SplashScreen(),
     );
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({ Key? key }) : super(key: key);
@@ -11,8 +12,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Splash Screen")
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset("assets/animations/loading.json"),
+          Text('Loading', style: TextStyle(fontFamily: 'RobotoBold', fontSize: 18),)
+        ],
       ),
     );
   }
